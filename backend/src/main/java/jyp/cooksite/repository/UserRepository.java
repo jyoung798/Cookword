9a41be90ac4a1063d042fb8c604e06abfa293067
jyoung <jyoung798@naver.com>
2020-07-05 22:36:37 +0900

    User repo 이메일 검색 수정
    이메일로 검색했을때 List가 아닌 단일값으로 나오도록 수정
    검색시 값이 안나올때 예외처리함

--- backend/src/main/java/jyp/cooksite/repository/UserRepository.java
+++ backend/src/main/java/jyp/cooksite/repository/UserRepository.java
@@ -3,8 +3,14 @@ package jyp.cooksite.repository;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.core.ParameterizedTypeReference;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 
 import jyp.cooksite.domain.user.User;
@@ -20,13 +26,17 @@ public class UserRepository {
 	}
 
 	public User findOne(Long id) {
+		
 		return em.find(User.class, id);
 	}
 
+	
+	//모든 사용자 유저를 검색하고 결과를 List 로 반환합니다.
 	public List<User> findAll() {
 		return em.createQuery("select u from User u", User.class).getResultList();
 	}
 
+	//사용자 이름을 통해서 검색하고 결과를 List 로 반환합니다.
 	public List<User> findByName(String name) {
 		return em.createQuery("select u from User u where u.name = :name", User.class)
 				.setParameter("name", name)
@@ -34,11 +44,19 @@ public class UserRepository {
 	}
 	
 	//email로 검색
-	public List<User> findByEmail(String email) {
+	public User findByEmail(String email) {
+		User user;
+		try {
+			 user = em.createQuery("select u from User u where u.email = :email", User.class)
+					.setParameter("email", email)
+					.getSingleResult();
+		} catch (NoResultException e) {
+			return null;
+		}
 		
-		return em.createQuery("select u from User u where u.email = :email", User.class)
-				.setParameter("email", email)
-				.getResultList();
+		return user;
+		
+	
 	} 
 	
 }
